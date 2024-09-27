6bbe8cf277a851d03137e11e57774e7deada3d29
pedrohfariaa <122329617+pedrohfariaa@users.noreply.github.com>
2024-09-27 15:48:15 -0300

    a

--- Mean-Variance-Standard Deviation Calculator/mean_var_std.py
+++ Mean-Variance-Standard Deviation Calculator/mean_var_std.py
@@ -1,84 +1,42 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 1,
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "[1 2 3 4 5 6 7 8 9]\n",
-      "{'mean': [[np.float64(4.0), np.float64(5.0), np.float64(6.0)], [np.float64(2.0), np.float64(5.0), np.float64(8.0)], np.float64(5.0)], 'variance': [[np.float64(6.0), np.float64(6.0), np.float64(6.0)], [np.float64(0.6666666666666666), np.float64(0.6666666666666666), np.float64(0.6666666666666666)], np.float64(6.666666666666667)], 'standard deviation': [[np.float64(2.449489742783178), np.float64(2.449489742783178), np.float64(2.449489742783178)], [np.float64(0.816496580927726), np.float64(0.816496580927726), np.float64(0.816496580927726)], np.float64(2.581988897471611)], 'max': [[np.int64(7), np.int64(8), np.int64(9)], [np.int64(3), np.int64(6), np.int64(9)], np.int64(9)], 'min': [[np.int64(1), np.int64(2), np.int64(3)], [np.int64(1), np.int64(4), np.int64(7)], np.int64(1)], 'sum': [[np.int64(12), np.int64(15), np.int64(18)], [np.int64(6), np.int64(15), np.int64(24)], np.int64(45)]}\n"
-     ]
-    }
-   ],
-   "source": [
-    "import numpy as np\n",
-    "\n",
-    "def calculate(lst):\n",
-    "        ls = np.array(lst)  \n",
-    "        print(ls)\n",
-    "\n",
-    "        \n",
-    "        mean_rows = [ls[[0, 1, 2]].mean(), ls[[3, 4, 5]].mean(), ls[[6, 7, 8]].mean()]\n",
-    "        mean_columns = [ls[[0, 3, 6]].mean(), ls[[1, 4, 7]].mean(), ls[[2, 5, 8]].mean()]\n",
-    "\n",
-    "        \n",
-    "        var_rows = [ls[[0, 1, 2]].var(), ls[[3, 4, 5]].var(), ls[[6, 7, 8]].var()]\n",
-    "        var_columns = [ls[[0, 3, 6]].var(), ls[[1, 4, 7]].var(), ls[[2, 5, 8]].var()]\n",
-    "\n",
-    "        \n",
-    "        std_rows = [ls[[0, 1, 2]].std(), ls[[3, 4, 5]].std(), ls[[6, 7, 8]].std()]\n",
-    "        std_columns = [ls[[0, 3, 6]].std(), ls[[1, 4, 7]].std(), ls[[2, 5, 8]].std()]\n",
-    "\n",
-    "        \n",
-    "        max_rows = [ls[[0, 1, 2]].max(), ls[[3, 4, 5]].max(), ls[[6, 7, 8]].max()]\n",
-    "        max_columns = [ls[[0, 3, 6]].max(), ls[[1, 4, 7]].max(), ls[[2, 5, 8]].max()]\n",
-    "\n",
-    "        \n",
-    "        min_rows = [ls[[0, 1, 2]].min(), ls[[3, 4, 5]].min(), ls[[6, 7, 8]].min()]\n",
-    "        min_columns = [ls[[0, 3, 6]].min(), ls[[1, 4, 7]].min(), ls[[2, 5, 8]].min()]\n",
-    "\n",
-    "        \n",
-    "        sum_rows = [ls[[0, 1, 2]].sum(), ls[[3, 4, 5]].sum(), ls[[6, 7, 8]].sum()]\n",
-    "        sum_columns = [ls[[0, 3, 6]].sum(), ls[[1, 4, 7]].sum(), ls[[2, 5, 8]].sum()]\n",
-    "\n",
-    "        return {\n",
-    "            'mean': [mean_columns, mean_rows, ls.mean()],\n",
-    "            'variance': [var_columns, var_rows, ls.var()],\n",
-    "            'standard deviation': [std_columns, std_rows, ls.std()],\n",
-    "            'max': [max_columns, max_rows, ls.max()],\n",
-    "            'min': [min_columns, min_rows, ls.min()],\n",
-    "            'sum': [sum_columns, sum_rows, ls.sum()],\n",
-    "        }\n",
-    "\n",
-    "    \n",
-    "result = calculate([1, 2, 3, 4, 5, 6, 7, 8, 9])\n",
-    "print(result)\n"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.12.4"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+import numpy as np
+
+def calculate(lst):
+        ls = np.array(lst)  
+        print(ls)
+
+        
+        mean_rows = [ls[[0, 1, 2]].mean(), ls[[3, 4, 5]].mean(), ls[[6, 7, 8]].mean()]
+        mean_columns = [ls[[0, 3, 6]].mean(), ls[[1, 4, 7]].mean(), ls[[2, 5, 8]].mean()]
+
+        
+        var_rows = [ls[[0, 1, 2]].var(), ls[[3, 4, 5]].var(), ls[[6, 7, 8]].var()]
+        var_columns = [ls[[0, 3, 6]].var(), ls[[1, 4, 7]].var(), ls[[2, 5, 8]].var()]
+
+        
+        std_rows = [ls[[0, 1, 2]].std(), ls[[3, 4, 5]].std(), ls[[6, 7, 8]].std()]
+        std_columns = [ls[[0, 3, 6]].std(), ls[[1, 4, 7]].std(), ls[[2, 5, 8]].std()]
+
+        
+        max_rows = [ls[[0, 1, 2]].max(), ls[[3, 4, 5]].max(), ls[[6, 7, 8]].max()]
+        max_columns = [ls[[0, 3, 6]].max(), ls[[1, 4, 7]].max(), ls[[2, 5, 8]].max()]
+
+        
+        min_rows = [ls[[0, 1, 2]].min(), ls[[3, 4, 5]].min(), ls[[6, 7, 8]].min()]
+        min_columns = [ls[[0, 3, 6]].min(), ls[[1, 4, 7]].min(), ls[[2, 5, 8]].min()]
+
+        
+        sum_rows = [ls[[0, 1, 2]].sum(), ls[[3, 4, 5]].sum(), ls[[6, 7, 8]].sum()]
+        sum_columns = [ls[[0, 3, 6]].sum(), ls[[1, 4, 7]].sum(), ls[[2, 5, 8]].sum()]
+
+        return {
+            'mean': [mean_columns, mean_rows, ls.mean()],
+            'variance': [var_columns, var_rows, ls.var()],
+            'standard deviation': [std_columns, std_rows, ls.std()],
+            'max': [max_columns, max_rows, ls.max()],
+            'min': [min_columns, min_rows, ls.min()],
+            'sum': [sum_columns, sum_rows, ls.sum()],
+        }
+
+    
+result = calculate([1, 2, 3, 4, 5, 6, 7, 8, 9])
+print(result)
--- Medical Data Visualizer/medical_data_visualizer.py
+++ Medical Data Visualizer/medical_data_visualizer.py
@@ -1,73 +1,40 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 9,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import pandas as pd\n",
-    "import seaborn as sns\n",
-    "import matplotlib.pyplot as plt\n",
-    "import numpy as np\n",
-    "\n",
-    "# 1\n",
-    "df = pd.read_csv('medical_examination.csv')\n",
-    "\n",
-    "\n",
-    "df['overweight'] = (df['weight'] / ((df['height'] / 100) **2 )). apply(lambda x : 1 if x >5 else 0)\n",
-    "df['cholesterol'] = df['cholesterol']. apply(lambda x : 0 if x == 1 else 1)\n",
-    "df['gluc'] = df['gluc']. apply(lambda x : 0 if x == 1 else 1)\n",
-    "\n",
-    "def fraw_cat_plot():\n",
-    "  df_cat = pd.melt(df, id_vars = ['cardio'], value_vars = ['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])\n",
-    "\n",
-    "  df_cat['total'] = 1\n",
-    "  df_cat = df_cat.groupby(['cardio', 'variable', 'value'], as_index = False).count()\n",
-    "\n",
-    "  fig = sns.catplot(x = 'variable', y = 'total', hue = 'value', col = 'cardio', data = df_cat, kind = 'bar').fig\n",
-    "  fig.savefig('catplot.png')\n",
-    "  return fig\n",
-    "\n",
-    "  def draw_heat_map():\n",
-    "   df_heat = df[\n",
-    "  (df['ap_lo'] <= df['ap_hi']) &\n",
-    "  (df['height'] >= df['height'].quantile(0.025)) &\n",
-    "  (df['height'] <= df['height'].quantile(0.975)) &\n",
-    "  (df['weight'] >= df['weight'].quantile(0.025)) &\n",
-    "  (df['weight'] <= df['weight'].quantile(0.975))]\n",
-    "\n",
-    "  corr = df_heat.corr(method='pearson')\n",
-    "\n",
-    "  mask = np.triu(corr)\n",
-    "\n",
-    "  fig, ax = plt.subplots(figsize=(12, 12))\n",
-    "\n",
-    "  sns.heatmap(cprr, linewidths=1, annot = True, mask = mask, fmt ='.lf', center =0.09,cbar_kws = {'shrink':0.5})\n",
-    "  fig.savefig('heatmap.png')\n",
-    "  return fig\n"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.12.4"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+# 1
+df = pd.read_csv('medical_examination.csv')
+
+
+df['overweight'] = (df['weight'] / ((df['height'] / 100) **2 )). apply(lambda x : 1 if x >5 else 0)
+df['cholesterol'] = df['cholesterol']. apply(lambda x : 0 if x == 1 else 1)
+df['gluc'] = df['gluc']. apply(lambda x : 0 if x == 1 else 1)
+
+def fraw_cat_plot():
+  df_cat = pd.melt(df, id_vars = ['cardio'], value_vars = ['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
+
+  df_cat['total'] = 1
+  df_cat = df_cat.groupby(['cardio', 'variable', 'value'], as_index = False).count()
+
+  fig = sns.catplot(x = 'variable', y = 'total', hue = 'value', col = 'cardio', data = df_cat, kind = 'bar').fig
+  fig.savefig('catplot.png')
+  return fig
+
+  def draw_heat_map():
+   df_heat = df[
+  (df['ap_lo'] <= df['ap_hi']) &
+  (df['height'] >= df['height'].quantile(0.025)) &
+  (df['height'] <= df['height'].quantile(0.975)) &
+  (df['weight'] >= df['weight'].quantile(0.025)) &
+  (df['weight'] <= df['weight'].quantile(0.975))]
+
+  corr = df_heat.corr(method='pearson')
+
+  mask = np.triu(corr)
+
+  fig, ax = plt.subplots(figsize=(12, 12))
+
+  sns.heatmap(cprr, linewidths=1, annot = True, mask = mask, fmt ='.lf', center =0.09,cbar_kws = {'shrink':0.5})
+  fig.savefig('heatmap.png')
+  return fig
